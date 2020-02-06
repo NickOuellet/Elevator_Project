b22bb780d1f6d6ddbc379502c20dd0d3542539b1
Nick Ouellet <nicolasouel@gmail.com>
2020-02-06 11:37:50 -0600

    First commit from terminal

---
+++ Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    // Elevator elevator = new Elevator();
+    // System.out.println(getName());
+
+  }
+}
